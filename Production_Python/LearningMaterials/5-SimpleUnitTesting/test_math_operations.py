c4b045b2d103a7047b224ae4052844f4d6df350b
David Cordero <davidcordero@kubrickgroup.com>
2025-11-04 09:52:15 +0000

    starting 5-unit testing

---
+++ Production_Python/LearningMaterials/5-SimpleUnitTesting/test_math_operations.py
@@ -0,0 +1,9 @@
+"""# test math operations.py"""
+
+def add(arg1, arg2):
+    return arg1 + arg2
+
+def test_add():
+    assert add(10, 20) == 30
+
+#assert raises an error if the statement is false.
